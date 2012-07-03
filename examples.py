c1b952dc16dc116195d8f5446a5c4864625c8851
Alex Musayev <alex.musayev@gmail.com>
2012-07-03 14:54:14 +0400

    Examples updated.

--- examples.py
+++ examples.py
@@ -6,8 +6,11 @@ import sys
 import glob
 import os.path
 import markdown
+
+# Extension module is imported for cases 2 and 3.
+# It's not necessary for the first example.
 import mdx_grid
-from pprint import pprint
+
 
 for fname in glob.glob('./examples/*.md'):
     sys.stdout.write("Processing '%s'... " % os.path.basename(fname))
@@ -17,32 +20,26 @@ for fname in glob.glob('./examples/*.md'):
     md.convertFile(fname, output=fname + '.bootstrap.html', encoding='utf8')
 
     # Case #2: Using one of the predefined alternative configuration profiles
+    config = mdx_grid.get_conf(mdx_grid.SKELETON_PROFILE)
     md = markdown.Markdown(extensions=['grid'],
-                           extension_configs={'grid': mdx_grid.get_conf(mdx_grid.SKELETON_PROFILE)})
+                           extension_configs={'grid': config})
     md.convertFile(fname, output=fname + '.skeleton.html', encoding='utf8')
 
-    # # Case #2: Using one of the predefined alternative configuration profiles
-    # conf = mdx_grid.GridConf.get_profile(mdx_grid.SKELETON_PROFILE)
-    # md = markdown.Markdown(extensions=['grid'],
-    #                        extension_configs={'grid': conf})
-    # md.convertFile(fname, output=fname + '.skeleton.html', encoding='utf8')
-
-    # # Case #3: Using custom extension configuration profile
-    # conf = {
-    #     'row_open': '<div class="custom-row">',
-    #     'row_close': '</div>',
-    #     'col_open': '<div class="{value}">',
-    #     'col_close': '</div>',
-    #     'col_span_class': 'custom-span{value}',
-    #     'col_offset_class': 'custom-offset{value}',
-    #     'default_col_class': 'default-span',
-    #     'common_col_class': 'common-col',
-    #     'col_class_first': 'first',
-    #     'col_class_last': 'last',
-    # }
-    # gridext = mdx_grid.GridExtension(configs=mdx_grid.SKELETON_PROFILE)
-    # md = markdown.Markdown(extensions=['grid'],
-    #                        extension_configs={'grid': conf})
-    # md.convertFile(fname, output=fname + '.custom.html', encoding='utf8')
+    # Case #3: Using custom extension configuration profile
+    config = {
+        'profile': 'custom',
+        'row_open': '<div class="row">',
+        'row_close': '</div>',
+        'col_open': '<div class="{value} column">',
+        'col_close': '</div>',
+        'default_col': 'span6',
+        'aliases': [
+            (r"\b(\d+)\:(\d+)\b", r"span\1 offset\2"),
+            (r"\b(\d+)\b", r"span\1"),
+        ],
+    }
+    md = markdown.Markdown(extensions=['grid'],
+                           extension_configs={'grid': config})
+    md.convertFile(fname, output=fname + '.custom.html', encoding='utf8')
 
     print("Done.")
