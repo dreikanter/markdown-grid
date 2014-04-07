0bc5398b13e91e50f373d4c2facd3a4f8b327c73
Alex Musayev <alex.musayev@gmail.com>
2014-04-07 12:43:20 +0400

    Classifiers added to the setup.

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-import sys
 from setuptools import setup
 
 
 setup(
     name='mdx_grid',
     description='Grid Extension for Python-Markdown',
-    version='0.2.1',
+    version='0.2.2',
     license='MIT',
     author='Alex Musayev',
     author_email='alex.musayev@gmail.com',
@@ -16,7 +15,16 @@ setup(
     package_dir={'mdx_grid': '.'},
     py_modules=['mdx_grid'],
     platforms=['any'],
-    install_requires=[
-        'markdown',
-    ],
+    install_requires=['markdown'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ]
 )
